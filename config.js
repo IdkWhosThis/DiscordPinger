dca3b1ad448f96cd1757ee82285b74cd46cf2f49
ImJustRuhan <86740889+ImJustRuhan@users.noreply.github.com>
2021-07-09 09:58:32 +0800

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+
+  "prefix": "gay", //Add your prefix here
+
+  "ownerOnly": false, //If set to true, only bot owner can control Pinger. If set to false, everyone can use Pinger.
+
+  "ownerID": "549595734568337408", //Id of bot owner for "ownerOnly" function.
+
+  "channelName": "", //Name of channel, where will be all pings sent.
+
+  "pingInterval": "1000", //Time in ms (miliseconds). 1000 recommended - If 1000ms (1s), bot will send ping every 1000ms (1s).
+
+  "token": "ODYyODY5NjA3NDg4NTUzMDEw.YOen8Q.ZT2rhgyseM1yXckHCk5iZ-nHA0Q" //Bot token from discord app.
+
+}
+
+module.exports = config;
